e9c17641b6ff92dedc66ed3f95a8ad35f6ab6d97
Hiram <hl.pi@aftership.com>
2021-07-18 12:00:52 +0800

    feat: implment f3

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const helloWorld = () => {
   console.log('Hello World!');
   console.log('I am feature PRO-001');
   console.log('I am feature PRO-002');
+  console.log('I am f3');
 }
 
 module.exports = helloWorld;
